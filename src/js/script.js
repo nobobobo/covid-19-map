c155e7a49a31933086e8be56d4a2bbe5ade38b10
nobobobo <ianhsu1221@gmail.com>
2020-05-12 19:16:24 -0700

    add interactive map using mapael

---
+++ src/js/script.js
@@ -0,0 +1,9 @@
+$(function () {
+    $(".mapcontainer").mapael({
+        map: {
+            // Set the name of the map to display
+            name: "world_countries"
+        }
+    });
+});
+console.log("hello")
