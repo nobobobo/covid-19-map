45057fbdf8dad5caca8966b1e022868f88309d87
nobobobo <ianhsu1221@gmail.com>
2020-05-12 19:30:29 -0700

    clean

--- src/js/script.js
+++
@@ -1,9 +0,0 @@
-$(function () {
-    $(".mapcontainer").mapael({
-        map: {
-            // Set the name of the map to display
-            name: "world_countries"
-        }
-    });
-});
-console.log("hello")
